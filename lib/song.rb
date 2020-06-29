12435a79628b002edc29f98b386b4bb9fa84eb6e
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-29 22:05:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,35 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def Song.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def Song.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def Song.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
+  def Song.find_by_name(name)
+    song = self.all.select {|song| song.name == name}
+    song[0]
+  end
+  
+  def Song.find_or_create_by_name(name)
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
+    end
+  end
 end
